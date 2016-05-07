1e9c31c311c2a12dcc7211a3fb2d557522675d1b
Brad <bradwat@live.com>
2016-05-07 00:45:15 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script>
+$('.dot-nav a').on('click', function() {
+
+    var scrollAnchor = $(this).attr('data-scroll'),
+        scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
+
+    $('.main').animate({
+        scrollTop: scrollPoint
+    }, 500);
+
+    return false;
+
+})
+
+
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 100) {
+        $('.dot-nav').addClass('fixed');
+        $('.main section').each(function(i) {
+            if ($(this).position().top <= windscroll) {
+                $('.dot-nav a.active').removeClass('active');
+                $('.dot-nav a').eq(i).addClass('active');
+            }
+        });
+
+    } else {
+
+        $('dot-nav').removeClass('fixed');
+        $('dot-nav a.active').removeClass('active');
+        $('dot-nav a:first').addClass('active');
+    }
+
+}).scroll();
+</script>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
